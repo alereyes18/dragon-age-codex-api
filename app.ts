52856174cfd6b2b8ec8ed17bfd2e9c8060e9f3c7
Alejandra Reyes <reyesalejandra161@gmail.com>
2024-11-02 02:57:49 -0400

    /

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import server from "./server";
 const host = process.env.HOST!;
-const port = process.env.PORT
+const port = parseInt(process.env.PORT!, 10);
 try {
 	server.listen({ host, port });
 	console.log(`Server listening on ${host}`);
--- build/app.js
+++ build/app.js
@@ -1,7 +1,8 @@
 import server from "./server";
 const host = process.env.HOST;
+const port = parseInt(process.env.PORT, 10);
 try {
-    server.listen({ host });
+    server.listen({ host, port });
     console.log(`Server listening on ${host}`);
 }
 catch (e) {
